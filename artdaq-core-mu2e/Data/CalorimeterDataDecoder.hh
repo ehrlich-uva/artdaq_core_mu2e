1b618c255ac256355362d2c0ae867fc69f7ec115
Paolo Girotti <giro94@libero.it>
2024-11-20 02:55:41 -0600

    Restored original CaloDecoder data packet for standard calo hits

--- artdaq-core-mu2e/Data/CalorimeterDataDecoder.hh
+++ artdaq-core-mu2e/Data/CalorimeterDataDecoder.hh
@@ -51,6 +51,7 @@ namespace mu2e {
         }
     };
 
+    // CalorimeterHitDataPacket: Each hit is readout as a variable length sequence of data packets
     struct CalorimeterHitDataPacket
     {
       uint16_t DetectorType : 3; // subdetector type e.g. CALO=0, CAPHRI = 1, TRAD = 2, LASER = 3
@@ -58,15 +59,28 @@ namespace mu2e {
       uint16_t ChannelNumber : 5; // channel ID from 0-19
       uint16_t DIRACA;
       uint16_t DIRACB;
+      uint16_t LastSampleMarkerStart : 12;
+      uint16_t LastSampleMarkerEnd : 12;
 
-      uint16_t LastSampleMarker; //0xFFF
+      // there are 4 types of sample length
+      uint16_t SampleType0  : 12;
+
+      uint16_t SampleType1A : 4;
+      uint16_t SampleType1B : 8;
+
+      uint16_t SampleType2A : 8;
+      uint16_t SampleType2B : 4;
+
+      uint16_t SampleType3A : 10;
+      uint16_t SampleType3B : 2;
+
       uint16_t ErrorFlags;
-      uint32_t Time;
-      uint16_t IndexOfMaxDigitizerSample;
-      uint16_t NumberOfSamples;
+      uint16_t Time;
+      uint8_t NumberOfSamples;
+      uint8_t IndexOfMaxDigitizerSample;
 
       CalorimeterHitDataPacket()
-	    : DetectorType(0), BoardID(0), ChannelNumber(0), DIRACA(0), DIRACB(0), LastSampleMarker(0), ErrorFlags(0), Time(0), IndexOfMaxDigitizerSample(0), NumberOfSamples(0) {}
+      : DetectorType(0), BoardID(0), ChannelNumber(0), DIRACA(0), DIRACB(0), ErrorFlags(0), Time(0), NumberOfSamples(0), IndexOfMaxDigitizerSample(0) {}
     };
 
     struct CalorimeterHitTestDataPacket
